336e4cf930dd35e53cdd8e19bd7c3e99e1eb0ab5
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 17:47:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -61,14 +61,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-  counter = 0
-  while counter < cart.length
-    if cart[counter]][:clearance]
-      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
-    end
-    counter += 1
-  end
-  cart
+
 end
 
 def checkout(cart, coupons)
