2bc989adb0b9ea6e9a2dede268a9c28627c7af15
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 17:39:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
     if collection[counter][:item] == name
       return collection[counter]
     end
-    collection_index += 1
+    counter += 1
   end
   nil
 end
