6ace698054fb2afd1a54bae848664db3c690d953
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 17:34:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,7 @@ def apply_coupons(cart, coupons)
           :item => couponed_item_name
           :price => coupons[counter][:cost] / coupons[counter][:num],
           :count => coupons[counter][:num]
-
+          :clearance => cart_item[:clearance]
         }
       end
     end
