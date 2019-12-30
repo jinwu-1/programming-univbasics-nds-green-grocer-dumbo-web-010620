96d13f7e2dc2854b1a2ce9ba4cca60bb1a87f69b
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 17:37:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -38,7 +38,7 @@ def apply_coupons(cart, coupons)
   counter = 0
   while counter < coupons.length
     cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
-    couponed_item_name = "#{coupons[counter][:item]} W/ COUPON"
+    couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[counter][:num]
       if cart_item_with_coupon
@@ -46,7 +46,7 @@ def apply_coupons(cart, coupons)
         cart_item[:count] -= coupons[counter][:num]
       else
         cart_item_with_coupon = {
-          :item => couponed_item_name
+          :item => couponed_item_name,
           :price => coupons[counter][:cost] / coupons[counter][:num],
           :count => coupons[counter][:num]
           :clearance => cart_item[:clearance]
