bfbcceef342c9e84989950fc5e23043d2459375b
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 16:21:29 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,13 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  collection_index = 0
+  while collection_index < collection.size do
+    current_item = collection[collection_index]
+    if (current_item[:item] == name)
+      current_item
+    end
+    collection_index += 1
+  end
+  nil
 end
 
 def consolidate_cart(cart)
